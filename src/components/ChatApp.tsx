0c9a5f8e4b5f17357a8b9aec27d70f798caac8d8
“Vijeet <vijeetbshah@gmail.com>
2024-01-30 22:16:44 +0530

    fix : made it mobile responsive

--- src/components/ChatApp.tsx
+++ src/components/ChatApp.tsx
@@ -13,7 +13,9 @@ const ChatApp: React.FC = () => {
   return (
     <div className="flex h-screen antialiased text-gray-800">
       <div className="flex flex-row h-full w-full overflow-x-hidden">
-        <Sidebar />
+        <div className="hidden md:flex">
+          <Sidebar />
+        </div>
 
         <div className="flex flex-col flex-auto h-full p-6">
           <div
